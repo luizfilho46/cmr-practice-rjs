fbf91fcba62df279af8cbae3e3a3cbb6cdafb0eb
luizfilho46 <luizcarlos.pereirafilho@yahoo.com.br>
2020-09-27 11:59:14 -0300

    criação da estrutura basica do index.js de entrada para renderizar paginas com reactjs

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const root = document.getElementById('root')
+
+const App = () => {
+    <h1>BoilerPlate ReactJS</h1>
+}
+
+ReactDom.render(<App />, root)
